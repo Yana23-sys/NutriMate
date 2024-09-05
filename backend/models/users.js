563478f7878d845ef52e7fa75f328569a3fc82d1
Yana Turiak <turyak1998@gmail.com>
2024-09-05 19:54:41 +0100

    create user schema

---
+++ backend/models/users.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    avatar: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    location: { type: String },
+})
+const User = mongoose.model('User', userSchema)
