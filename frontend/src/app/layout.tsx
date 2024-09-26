2daf6aa7a621087fc54e7dd5c01012f44315d801
Yana Turiak <turyak1998@gmail.com>
2024-09-26 21:36:03 +0100

    setup frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+
+const RootLayout = ({ children }: Readonly<{children: React.ReactNode}>) => {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout
