7f97e92f0c7b4273775b381e60bf8cd64df205bc
Yana Turiak <turyak1998@gmail.com>
2024-09-27 18:15:48 +0100

    create header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,9 @@ import "../styles/globals.css";
 const RootLayout = ({ children }: Readonly<{children: React.ReactNode}>) => {
   return (
     <html lang="en">
+      <head>
+        <title>NutriMate</title>
+      </head>
       <body>
         {children}
       </body>
