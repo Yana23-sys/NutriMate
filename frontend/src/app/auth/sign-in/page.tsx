4405d76d2556cfa8e1c34fadf5a86db6fa545b30
Yana Turiak <turyak1998@gmail.com>
2024-09-28 18:38:13 +0100

    Fix sign-in page

--- frontend/src/app/auth/sign-in/page.tsx
+++ frontend/src/app/auth/sign-in/page.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { login } from '@/store/auth/slice'
 
 
